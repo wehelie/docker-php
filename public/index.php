bbd3aeb185bfb5bac68fe1761b3647f7baaec106
Leyth <mawehelie@gmail.com>
2019-02-22 00:14:14 -0800

    introduction to docker

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->write("Hello, World!");
+
+    return $response;
+});
+
+// $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+//     $name = $args['name'];
+//     $response->getBody()->write("Hello, $name");
+
+//     return $response;
+// });
+
+$app->run();
